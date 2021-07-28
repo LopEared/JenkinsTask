7f8e24d048c6b8b87e7a28035addd310e2e5f474
LopEared <40773335+LopEared@users.noreply.github.com>
2021-07-28 11:27:12 +0300

    Update jenkins2.groovy

--- jenkins2.groovy
+++ jenkins2.groovy
@@ -124,14 +124,17 @@ pipeline {
                     echo
                     #docker run --rm -d --name ExperCat -p 4040:8080 tomcat:8.5.38
                     docker run --rm -d --name "ServiceCurrent-$BUILD_NUMBER" -p 4040:7070 "$ImageName-$BUILD_NUMBER:$BUILD_NUMBER" 
+                    curl http://172.18.144.193:4040/
                     echo
                     docker ps
                     echo
                     docker ps -a
                     echo
-                    #docker stop "ServiceCurrent-$BUILD_NUMBER"
+                    docker stop "ServiceCurrent-$BUILD_NUMBER"
                     echo
                     docker image prune
+                    echo
+                    docker rmi -f "$ImageName-$BUILD_NUMBER:$BUILD_NUMBER"
                     echo
                     docker images
                 '''
